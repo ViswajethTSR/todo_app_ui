4c20bc1ff6a6ab017e844aad8549de6c369eeeed
viswajeth <tsrviswajeth@gmail.com>
2023-11-13 10:41:17 +0530

    Basic todo app is ready

---
+++ lib/custom_designs/border.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomShapeClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0, size.height - 40);
+    path.quadraticBezierTo(
+        size.width / 4, size.height, size.width / 2, size.height);
+    path.quadraticBezierTo(
+        3 / 4 * size.width, size.height, size.width, size.height - 40);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
