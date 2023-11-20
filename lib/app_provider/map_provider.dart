04244bea94e4ba318fecbdd9c279fca9f7b98a1f
viswajeth <tsrviswajeth@gmail.com>
2023-11-20 15:24:59 +0530

    Drawing polyline in the map according to the datas from web socket using Provider State management

--- lib/app_provider/map_provider.dart
+++ lib/app_provider/map_provider.dart
@@ -1,3 +1,78 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
 
-class MapProvider extends ChangeNotifier {}
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapProvider extends ChangeNotifier {
+  late Completer<GoogleMapController> _controller;
+  late Marker _marker;
+  List<LatLng> _polylinePoints = [];
+  late LatLng _currentPosition;
+  bool _mapLoaded = false;
+
+  MapProvider() {
+    _controller = Completer();
+    _currentPosition = LatLng(21.1458, 79.0882);
+    _marker = Marker(
+      markerId: MarkerId('1'),
+      position: _currentPosition,
+      infoWindow: InfoWindow(title: 'Marker'),
+    );
+  }
+
+  Future<void> updateMarkerAndPolyline(LatLng newPosition) async {
+    final GoogleMapController controller = await _controller.future;
+
+    // Animate the movement over 2 seconds
+    controller.animateCamera(CameraUpdate.newLatLng(newPosition));
+
+    final int steps = 50;
+    final double latStep = (newPosition.latitude - _currentPosition.latitude) / steps;
+    final double lngStep = (newPosition.longitude - _currentPosition.longitude) / steps;
+
+    for (int i = 1; i <= steps; i++) {
+      await Future.delayed(Duration(milliseconds: (2000 / steps).round()));
+
+      _marker = _marker.copyWith(
+        positionParam: LatLng(
+          _currentPosition.latitude + i * latStep,
+          _currentPosition.longitude + i * lngStep,
+        ),
+      );
+
+      _polylinePoints.add(_marker.position);
+      notifyListeners();
+    }
+
+    _currentPosition = newPosition;
+    notifyListeners();
+  }
+
+  Set<Polyline> buildPolyline() {
+    if (_polylinePoints.length < 2) {
+      return Set();
+    }
+
+    return {
+      Polyline(
+        polylineId: PolylineId('polyline'),
+        color: Colors.blue,
+        width: 5,
+        points: _polylinePoints,
+      )
+    };
+  }
+
+  void setMapLoaded() {
+    _mapLoaded = true;
+    notifyListeners();
+  }
+
+  bool get isMapLoaded => _mapLoaded;
+
+  Marker get marker => _marker;
+
+  LatLng get currentPosition => _currentPosition;
+
+  Completer<GoogleMapController> get controller => _controller;
+}
--- lib/provider_screens/provider_map_page.dart
+++ lib/provider_screens/provider_map_page.dart
@@ -1,15 +1,122 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:provider/provider.dart';
+
+import 'package:viswa_todo_app/app_provider/map_provider.dart';
+
+import '../custom_designs/custom_app_bar.dart';
+import '../screens/web_socket.dart';
+
+final channel = IOWebSocketChannel.connect('ws://$wsIP:$wsPORT');
 
 class ProviderMapPage extends StatefulWidget {
-  const ProviderMapPage({super.key});
-
   @override
-  State<ProviderMapPage> createState() => _ProviderMapPageState();
+  _ProviderMapPageState createState() => _ProviderMapPageState();
 }
 
 class _ProviderMapPageState extends State<ProviderMapPage> {
+  late MapProvider _mapProvider;
+  final IOWebSocketChannel channel = IOWebSocketChannel.connect('ws://$wsIP:$wsPORT');
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ChangeNotifierProvider(
+      create: (context) => MapProvider(),
+      child: Scaffold(
+        appBar: null,
+        body: Stack(
+          children: [
+            MapContent(channel: channel),
+            Consumer<MapProvider>(
+              builder: (context, model, child) {
+                if (!model.isMapLoaded) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return SizedBox.shrink();
+              },
+            ),
+            buildAppBarForMaps("Provider Map Page"),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40, 65, 0, 0),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _mapProvider = Provider.of<MapProvider>(context, listen: false);
+  }
+
+  @override
+  void dispose() {
+    _mapProvider.dispose(); // Dispose resources in MapProvider
+    channel.sink.close();
+    super.dispose();
+  }
+}
+
+class MapContent extends StatelessWidget {
+  final IOWebSocketChannel channel;
+
+  MapContent({required this.channel});
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<MapProvider>(context);
+
+    return GoogleMap(
+      markers: Set.of([model.marker]),
+      polylines: model.buildPolyline(),
+      initialCameraPosition: CameraPosition(
+        target: model.currentPosition,
+        zoom: 12.0,
+      ),
+      onMapCreated: (GoogleMapController controller) {
+        model.controller.complete(controller);
+        model.setMapLoaded();
+        startListeningToWebSocket(model, channel);
+      },
+    );
+  }
+
+  void startListeningToWebSocket(MapProvider model, IOWebSocketChannel channel) {
+    channel.stream.listen(
+      (data) {
+        if (model.isMapLoaded) {
+          print('Received WebSocket data: $data');
+          try {
+            Map<String, dynamic> location = jsonDecode(data);
+            print('Decoded location: $location');
+            model.updateMarkerAndPolyline(LatLng(location['latitude'], location['longitude']));
+          } catch (e) {
+            print('Error decoding JSON: $e');
+          }
+        }
+      },
+      onError: (error) {
+        print('WebSocket error: $error');
+      },
+      onDone: () {
+        print('WebSocket channel closed.');
+      },
+    );
   }
 }
